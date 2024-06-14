699de9852e309759e975f270ba0af1a4546a8fff
shubh <shubhamchintalwar1227@gmial.com>
2024-06-14 18:04:46 +0530

    changes add

--- src/test/java/practiceGit/GiitPush.java
+++ src/test/java/practiceGit/GiitPush.java
@@ -3,7 +3,8 @@ package practiceGit;
 public class GiitPush {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+		System.out.println("test1 uesd for test");
 
 	}
 
---
+++ src/test/java/practiceGit/PaymentClass.java
@@ -0,0 +1,10 @@
+package practiceGit;
+
+public class PaymentClass {
+
+	public static void main(String[] args) {
+		
+		System.out.println("this is payment Page");
+	}
+
+}
