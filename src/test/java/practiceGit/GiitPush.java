c567547aeec3622321ae9fa39d8e110f1555590c
shubh <shubhamchintalwar1227@gmial.com>
2024-06-14 18:13:40 +0530

    new default classs added

--- src/test/java/practiceGit/GiitPush.java
+++ src/test/java/practiceGit/GiitPush.java
@@ -3,7 +3,7 @@ package practiceGit;
 public class GiitPush {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("this is git push class");
 
 	}
 
